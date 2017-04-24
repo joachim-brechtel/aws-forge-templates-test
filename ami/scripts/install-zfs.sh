89c0985c8810dc26f9142d256d5b26c007ff1cf5
Felix Haehnel <fhaehnel@atlassian.com>
2017-04-24 15:35:26 +1000

    BSERVDEV-14785: Pin AMI baseimage
    The Amazon Linux AMI, which is the base AMI for our AMI, has been
    updated to use a newer Linux kernel. Unfortunately this kernel is not
    supported by the currently available version of ZFS on Linux.
    In order to maintain compatibility, this change pins the version of the
    base AMI and restricts the `yum update` to only security patches.
    
    Once v6.5.10 of ZoL has been released and made available in the RPM,
    all the version pins can be removed.

--- ami/scripts/install-zfs.sh
+++ ami/scripts/install-zfs.sh
@@ -4,10 +4,15 @@ set -e
 if [[ "${APP_DATA_FS_TYPE}" = "zfs" ]]; then
     echo "Installing ZFS"
 
+    # Because we're not able to run on the latest Kernel, we need to manually install the 
+    # sources to avoid pulling down the latest (and incompatible) sources transitively.
+    # This can be removed when we unlock the version, which should be possible when ZFS on Linux 6.5.10 is released.
+    sudo yum install --releasever=2016.09 -y "kernel-devel-$(uname -r)"
+
     sudo yum localinstall -y --nogpgcheck http://download.zfsonlinux.org/epel/zfs-release.el6.noarch.rpm
- 
     sudo gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
     sudo yum install -y zfs
+    sudo /sbin/modprobe zfs
 
     # Enable ZFS to start at boot
     echo "Enabling ZFS"
