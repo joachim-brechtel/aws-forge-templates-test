84d7763191093c548bd4e6395dd5941bb3948ccb
Nhan Dang <nhdang@atlassian.com>
2017-12-07 14:46:42 +1100

    CONFSERVER-54350 pass ATL_USE_COLLECTD from cfn

--- ami/scripts/init.d/atl-init-confluence.sh
+++ ami/scripts/init.d/atl-init-confluence.sh
@@ -190,15 +190,13 @@ function configureNginx {
 function installConfluence {
     atl_log "Checking if ${ATL_CONFLUENCE_SHORT_DISPLAY_NAME} has already been installed"
 
-    atl_log "Creating file /etc/ld.so.conf.d/confluence.conf"
-    echo /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/lib/amd64/server/ > /etc/ld.so.conf.d/confluence.conf
-    sudo ldconfig
-
-    if [ -e /etc/init.d/collectd ]; then
+    if [ "true" = "${ATL_USE_COLLECTD}" ] && [ -e /etc/init.d/collectd ]; then
+        atl_log "Creating file /etc/ld.so.conf.d/confluence.conf"
+        echo /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/lib/amd64/server/ > /etc/ld.so.conf.d/confluence.conf
+        sudo ldconfig
         service collectd restart
+        atl_log "Creating file /etc/ld.so.conf.d/confluence.conf ==> done"
     fi
-
-    atl_log "Creating file /etc/ld.so.conf.d/confluence.conf ==> done"
 
     if [[ -d "${ATL_CONFLUENCE_INSTALL_DIR}" ]]; then
         local ERROR_MESSAGE="${ATL_CONFLUENCE_SHORT_DISPLAY_NAME} install directory ${ATL_CONFLUENCE_INSTALL_DIR} already exists - aborting installation"
