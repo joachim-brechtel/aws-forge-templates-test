57589b7b98df034e62670d433862a5bc86bc55af
Richard Friend <rfriend@atlassian.com>
2018-01-08 20:54:02 +1100

    BSERVDEV-16643 update to kernel with page table isolation enabled to mitigate metldown/spectre

--- ami/scripts/install-zfs.sh
+++ ami/scripts/install-zfs.sh
@@ -7,9 +7,9 @@ if [[ "${APP_DATA_FS_TYPE}" = "zfs" ]]; then
     # Because we're not able to run on the latest Kernel, we need to manually install the 
     # sources to avoid pulling down the latest (and incompatible) sources transitively.
     # This can be removed when we unlock the version, which should be possible when ZFS on Linux 6.5.10 is released.
-    sudo yum install --releasever=2016.09 -y "kernel-devel-$(uname -r)"
+    sudo yum install -y "kernel-devel-$(uname -r)"
 
-    sudo yum localinstall -y --nogpgcheck http://download.zfsonlinux.org/epel/zfs-release.el6.noarch.rpm
+    sudo yum localinstall -y http://download.zfsonlinux.org/epel/zfs-release.el6.noarch.rpm
     sudo gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
     sudo yum install -y zfs
     sudo /sbin/modprobe zfs
