c62eff4b11fb650a690b9ef3e9ffe0d9048731dd
Adam Brokes <abrokes@atlassian.com>
2018-11-19 10:56:02 +1100

    DCD-16: Add support for validating quickstarts

--- test/validate-template.sh
+++ test/validate-template.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Validate Cloudformation templates that are located in /templates and /quickstart folders.
+# The script requires AWS credentials to be set in the shell and connectivity to AWS.
+#==============================================================================
 set -e
 
 BASEDIR=$(dirname $0)
@@ -7,7 +10,12 @@ cd ${BASEDIR}/..
 ABSOLUTE_AWS_DIR=$(pwd)
 
 TEMPLATE_NAME=${1:?"A template name must be specified."}
-TEMPLATES_PATH="${ABSOLUTE_AWS_DIR}/templates"
+
+if [[ "$TEMPLATE_NAME" == *"quickstart-"* ]]; then
+    TEMPLATES_PATH="${ABSOLUTE_AWS_DIR}/quickstarts"
+else
+    TEMPLATES_PATH="${ABSOLUTE_AWS_DIR}/templates"
+fi
 
 echo "Validating ${TEMPLATE_NAME}..."
 aws cloudformation validate-template --template-body "file://${TEMPLATES_PATH}/${TEMPLATE_NAME}"
