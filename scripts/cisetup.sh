209574c6522807107cac9d3946dba553a469fc9a
Varun Arbatti <varbatti@atlassian.com>
2019-03-07 14:12:49 +1100

    Revert "ITOPS-1461: Removes ssh-add command from ci-setup script"
    This reverts commit 219187e5e42288b81cc988937b1476a3c49e1708.

--- scripts/cisetup.sh
+++ scripts/cisetup.sh
@@ -6,6 +6,8 @@ set -e
 if [[ ! -z "$CI_TASKCAT_USER_GITHUB_PRIVATE_KEY" ]] ; then
     echo "Adding Github private key"
     $(umask  077; echo ${CI_TASKCAT_USER_GITHUB_PRIVATE_KEY} | base64 -d > ~/.ssh/github_id_rsa)
+    eval "$(ssh-agent)"
+    ssh-add ~/.ssh/github_id_rsa
 fi
 
 chmod 744 ./scripts/bootstrap && ./scripts/bootstrap --no-recurse
